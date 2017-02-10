978215e46b85dc2cc17977693f93c39fc35c2692
joneszj <joneszj@gmail.com>
2017-02-10 15:37:07 -0500

    signalr webforms

---
+++ SignalR_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignalR_MVC.Startup))]
+namespace SignalR_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
